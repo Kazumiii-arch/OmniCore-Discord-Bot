9d56cab265731b05f958865bb723232ff78543b1
Kazumiii-arch <weskyboundrealms@gmail.com>
2025-07-11 20:01:24 +0800

    Update community.py

--- cogs/community.py
+++ cogs/community.py
@@ -1,1 +1,13 @@
+from discord.ext import commands
 
+class Community(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def level(self, ctx):
+        """Checks your current level (placeholder)."""
+        await ctx.send(f"{ctx.author.mention}, you are currently level 1! (Feature coming soon!)")
+
+async def setup(bot):
+    await bot.add_cog(Community(bot))
