d20d179bf4e68bc585ef2ad817fd833f5afc5410
Kazumiii-arch <weskyboundrealms@gmail.com>
2025-07-11 20:02:15 +0800

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
 
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Checks the bot's latency."""
+        latency = round(self.bot.latency * 1000)
+        await ctx.send(f"Pong! Latency is {latency}ms.")
+
+async def setup(bot):
+    await bot.add_cog(Utility(bot))
