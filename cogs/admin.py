493c05622680a2bac37cf5b8bde79971ca8f1540
Kazumiii-arch <weskyboundrealms@gmail.com>
2025-07-11 20:00:42 +0800

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,1 +1,19 @@
+from discord.ext import commands
 
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Admin Cog is ready.")
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: commands.MemberConverter, *, reason="No reason provided."):
+        """Kicks a member from the server."""
+        await member.kick(reason=reason)
+        await ctx.send(f"Kicked {member.mention}. Reason: {reason}")
+
+async def setup(bot):
+    await bot.add_cog(Admin(bot))
