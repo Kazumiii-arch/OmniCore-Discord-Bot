4a5eb90c7c7395e715a7718c6336f1e872266c9d
Kazumiii-arch <weskyboundrealms@gmail.com>
2025-07-11 20:01:42 +0800

    Update productivity.py

--- cogs/productivity.py
+++ cogs/productivity.py
@@ -1,1 +1,8 @@
+from discord.ext import commands
 
+class Productivity(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+async def setup(bot):
+    await bot.add_cog(Productivity(bot))
